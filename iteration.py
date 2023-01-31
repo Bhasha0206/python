3a0b6d027d2f0ee274f6aad5b45455d5869f16c5
Madina Bhasha <95214222+Bhasha0206@users.noreply.github.com>
2023-01-31 13:31:27 +0530

    Add files via upload

--- iteration.py
+++ iteration.py
@@ -12,5 +12,5 @@
 #     print(chr(i),end=" ")
 
 for i in range(90,64,-2):
-    if i!=65 and i!=69 and i!=73 and i!=79 and i!=85:
+    if i==65 and i!=69 and i!=73 and i!=79 and i!=85:
         print(chr(i),end=" ")
